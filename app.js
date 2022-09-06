46116d912b786187682316b3926d307b593a898f
Abhijit Ghosh <abhijitghosh916@gmail.com>
2022-09-07 00:01:01 +0530

    media-queries added

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
-const button = document.querySelector("#check-button");
+const checkButton = document.querySelector("#check-button");
+const errorMesssage = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-button.addEventListener("click", function () {
-  alert("clicked");
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+  hideMessage();
+  if (billAmount.value > 0) {
+    if (billAmount >= cashGiven) {
+      const billAmountToBeReturned = cashGiven.value - billAmount.value;
+      calculateChange(billAmountToBeReturned);
+    } else {
+      showErrorMessage("cash provided should be greater than the bill amount");
+    }
+  } else {
+    showErrorMessage("invalid Bill Amount");
+  }
 });
+
+function hideMessage() {
+  errorMesssage.style.dispaly = "none";
+}
+
+function calculateChange(billAmountToBeReturned) {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(
+      billAmountToBeReturned / availableNotes[i]
+    );
+    billAmountToBeReturned %= availableNotes[i];
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function showErrorMessage(message) {
+  errorMesssage.style.dispaly = "block";
+  errorMesssage.innerText = message;
+}
