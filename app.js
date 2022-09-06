84044e1f24ea7502bb8e217774970eb5f6a99691
Abhijit Ghosh <abhijitghosh916@gmail.com>
2022-09-06 11:09:09 +0530

    html, css, and js added

---
+++ app.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const button = document.querySelector("#check-button");
+
+button.addEventListener("click", function () {
+  alert("clicked");
+});
